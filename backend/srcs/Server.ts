8cd0533e4777b37010596f5fa0d7b573e17e3d9a
jerem <jeremcombe@hotmail.fr>
2024-04-23 12:01:33 +0200

    test cors opt

--- backend/srcs/Server.ts
+++ backend/srcs/Server.ts
@@ -30,11 +30,11 @@ export class Server extends DataBase {
   }
 
   startApp(): void {
-    const corsOptions: cors.CorsOptions = {
+  /*  const corsOptions: cors.CorsOptions = {
       origin: "*",
       optionsSuccessStatus: 200,
-    };
-    app.use(cors(corsOptions));
+    };*/
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.listen(port, () => {
