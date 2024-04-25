9fbf5f034487bc00a951fdca2ff744699c1a0cec
jerem <jeremcombe@hotmail.fr>
2024-04-25 10:45:26 +0200

    test https

--- frontend/utils/server.js
+++ frontend/utils/server.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://jeremy.training.real-estate-executive.com';
+const url = 'http://13.36.191.244:8000/';
 
 export const fetchAllLogs = async () => {
     try {
