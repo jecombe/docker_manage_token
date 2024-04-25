ec4931b8d63569befd0e88ca0788827d189c196a
jerem <jeremcombe@hotmail.fr>
2024-04-25 10:14:14 +0200

    test https

--- backend/srcs/Server.ts
+++ backend/srcs/Server.ts
@@ -31,11 +31,12 @@ export class Server extends DataBase {
 
   startApp(): void {
     const corsOptions: cors.CorsOptions = {
-      origin: "*", // Autorise les requêtes depuis n'importe quelle origine
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Autorise les méthodes HTTP spécifiées
+      origin: "*", 
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       preflightContinue: false,
       optionsSuccessStatus: 204,
     };
+    
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
