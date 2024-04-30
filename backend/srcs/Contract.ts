467ceca734d98d47d3e9b0db08063048e71c0c94
jerem <jeremcombe@hotmail.fr>
2024-04-30 16:59:50 +0200

    debug

--- backend/srcs/Contract.ts
+++ backend/srcs/Contract.ts
@@ -104,7 +104,7 @@ export class Contract extends Viem {
 
   async sendVolumeDaily(volume: number): Promise<void> {
 
-    console.log("HOHOHOHOHOH", typeof this.saveTime, typeof this.timeVolume);
+    console.log("HOHOHOHOHOH", typeof this.saveTime[0], typeof this.timeVolume);
 
     if (this.timeVolume && !_.includes(this.saveTime, this.timeVolume)) {
       this.manager.sendWsVolumeToAllClients({timestamp: this.timeVolume, volume: `${volume}`})
